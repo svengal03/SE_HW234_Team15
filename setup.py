2702bb9ca52f3ee677de9ada00c41c8bbd80c64f
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-08-29 19:48:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='SE Homework1',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Good Git Repo For Homework 1',
+    tests_require=['pytest'],
+    author='Chirumamilla, Raviteja',
+    author_email='raviteja.chirumamilla@gmail.com',
+    url='https://github.com/irkingmaker/se_project',
+    python_requires='>=3.7',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: Homework1",
+    ],
+    license='MIT'
+)
