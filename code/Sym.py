ba9b4ccad98ebc66535ab230548575ad083b181f
svengal03 <112122632+svengal03@users.noreply.github.com>
2022-09-05 23:36:46 -0400

    Create Sym.py

---
+++ code/Sym.py
@@ -0,0 +1,27 @@
+import math
+class Sym:
+    def __int__(self,c_name,c_position):
+        self.n = 0 ##characters seen
+        self.at = c_position or 0 ##position of column
+        self.name = c_name or ""  ## name of a column
+        self._has = dict() ##dictionary to contain freq of a symbol
+    def add(self,k):
+        if k != '?':
+            self.n +=1
+            if k in self._has:
+                self._has[k] +=1
+            else:
+                self._has[k] =1
+    
+    def mid(self):
+        mode = max(self._has, key = self._has.get)
+        return mode
+    
+    def div(self):
+        e=0
+        def func(p):
+            return  p*math.log(p,2)
+        for v in self._has.values():
+            if v> 0:
+                e = e-func(v/self.n)
+        return e
