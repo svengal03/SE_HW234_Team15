37dbd99e2ad4506b9a6ac4a6da56ec418f92b1bf
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-09-06 16:24:39 -0400

    Update Num.py

--- code/Num.py
+++ code/Num.py
@@ -1,16 +1,18 @@
-from code import Utils
 import math
 import random
+from Utils import *
+import Utils
 
 class Num:
-    def __init__(self, c = 0 , s = "" ):
-        self.n = 0                  # items seen
-        self.at = c      # column  position
-        self.name = s         # column name
-        self._has = []          # kept data
-        self.lo = float('inf')           # lowest seen
-        self.hi = -float('inf')          # highest seen
-        self.isSorted = True # no updates since last sort of data
+    def __init__(self, c=0, s=""):
+        self.n = 0  # items seen
+        self.at = c  # column  position
+        self.name = s  # column name
+        self._has = {}  # kept data
+        self.lo = float('inf')  # lowest seen
+        self.hi = -float('inf')  # highest seen
+        self.isSorted = True  # no updates since last sort of data
+    
 
     def nums(self):
         return self._has
@@ -20,11 +22,18 @@ class Num:
             self.n += 1
             self.lo = min(self.lo, v)
             self.hi = max(self.hi, v)
-    
-    def div(self,a):
+            if len(self._has) < Utils.the['nums']:
+                pos = 1 + len(self._has)
+            elif random.random() < Utils.the['nums'] / self.n:
+                pos = random.randint(1, len(self._has))
+            if pos:
+                self.isSorted = False
+                self._has[pos] = int(v)
+
+    def div(self):
         a = self.nums()
-        return (Utils.per(a,0.9) - Utils.per(a,0.1)) / 2.58
+        return (per(a, 0.9) - per(a, 0.1)) / 2.58
 
     def mid(self):
-        return Utils.per(self.nums(),0.5)
-
+        print(self.nums())
+        return per(self.nums(), 0.5)
