a6f920e92f6b752bcc0b0ed6a16005ed0097e3e7
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-09-06 03:23:32 -0400

    Update Sym.py

--- code/Sym.py
+++ code/Sym.py
@@ -1,8 +1,7 @@
 import math
 class Sym:
-    def __int__(self,n=0,c_name = "" ,c_position = 0):
-        
-        self.n = n
+    def __int__(self,c_name = "" ,c_position = 0):        
+        self.n = 0
         #characters seen
         self.at = c_position 
         # position of column
@@ -11,7 +10,6 @@ class Sym:
         self._has = dict() # dictionary to contain freq of a symbol
         
     def add(self,k):
-        
         if k != '?':
             self.n +=1
             if k in self._has:
@@ -20,12 +18,10 @@ class Sym:
                 self._has[k] =1
     
     def mid(self):
-        
         mode = max(self._has, key = self._has.get)
         return mode
     
     def div(self):
-        
         e=0
         def func(p):
             return  p*math.log(p,2)
