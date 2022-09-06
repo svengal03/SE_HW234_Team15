fcebde2b3d30bee0608cb8001e8893a84a9a40c1
dnellur4 <112123327+dnellur4@users.noreply.github.com>
2022-09-06 01:09:57 -0400

    Update Num.py

--- code/Num.py
+++ code/Num.py
@@ -1,1 +1,26 @@
+class Num:
+    def __init__(self, c, s):
+        self.n = 0                  # items seen
+        self.at = c if c else 0     # column  position
+        self.name = s or ""         # column name
+        self._has = []          # kept data
+        self.lo = float('inf')           # lowest seen
+        self.hi = -float('inf')          # highest seen
+        self.isSorted = True # no updates since last sort of data
+
+    def nums(self):
+        return self._has
+
+    def add(self, v):
+        if v != "?":
+            self.n += 1
+            self.lo = min(self.lo, v)
+            self.hi = max(self.hi, v)
+    
+    def div(self,a):
+        a = self.nums()
+        return (per(a,0.9) - per(a,0.1)) / 2.58
+
+    def mid(self):
+        return per(self.nums(),0.5)
 
