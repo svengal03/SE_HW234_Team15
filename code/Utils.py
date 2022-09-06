ca1e54447ead220af3fe6061db47754caf36afae
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-09-06 02:30:38 -0400

    Update Utils.py

--- code/Utils.py
+++ code/Utils.py
@@ -7,3 +7,26 @@ def per(t,p):
 def oo(t):
     print(str(t))
     return t
+
+def msg(status):
+    return "PASS" if status else "FAIL"
+
+def coerce(s):
+    def fun(s1):
+        if s1 == "true":
+            return True
+        if s1 == "false":
+            return False
+        return s1
+    return int(s) if s.isnumeric() else None or fun(s)
+
+
+def create_the():
+    the = {}
+    tup_list = re.findall(r'[-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)', help)
+
+    for k,x in tup_list:
+        the[k] = coerce(x)
+    return the
+
+the = create_the()
