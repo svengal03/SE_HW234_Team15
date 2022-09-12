58cb36dd8a6cac2968475ba43539ab8d71f54c4f
dnellur4 <112123327+dnellur4@users.noreply.github.com>
2022-09-12 13:21:45 -0400

    Update Row.py

--- code/Row.py
+++ code/Row.py
@@ -1,9 +1,10 @@
 from Utils import copy
 
 class Row:
-  def __init__(self, t):
+  def __init__(self, t, data):
     self.cells = t
     self.cooked = copy(t)
     self.isEvaled = False
+    self.outerSpace= data
 
-    return self.cells, self.cooked, self.isEvaled
+    return self.cells, self.cooked, self.isEvaled, self.outerSpace
