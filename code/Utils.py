56b01c4e76307ae59f18195358d9ce05680dfb42
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-09-19 11:09:36 -0400

    Update Utils.py

--- code/Utils.py
+++ code/Utils.py
@@ -67,12 +67,11 @@ def coerce(s):
                 return False
             return re.match("\s*(.*)\s*", s).string
 
-def parse_csv(s, func=None, separator=','):
-    lines = src.split('\n')
-    with open(s, "r") as src:
+def parse_csv(fname, fun, sep=','):
+    with open(fname, "r") as src:
         lines_csv = src.readlines()
         for line in lines_csv:
-            row = line.split(separator)
+            row = line.split(sep)
             fun(row)
         
 
